112338c7dd2cabe3975faa483a00f282e57d19cd
arunsetty <arunsetty1729@gmail.com>
2024-07-26 11:18:06 +0530

    added internal response

--- src/types/response.ts
+++ src/types/response.ts
@@ -29,3 +29,31 @@ export type DEFAULT_RES_ARR<T> =
     };
 
 export type DEFAULT_RES_ARR_P<T> = Promise<DEFAULT_RES_ARR<T>>;
+
+export type INTERNAL_RES_SINGLE<T> =
+    {
+        success: false,
+        errors: string[],
+        data?: T,
+        warnings?: string[],
+    } | {
+        success: true,
+        data: T,
+        warnings?: string[],
+    };
+
+export type INTERNAL_RES_SINGLE_P<T> = Promise<INTERNAL_RES_SINGLE<T>>;
+
+export type INTERNAL_RES_ARR<T> =
+    {
+        success: false,
+        errors: string[],
+        data?: T[],
+        warnings?: string[],
+    } | {
+        success: true,
+        data: T[],
+        warnings?: string[],
+    };
+
+export type INTERNAL_RES_ARR_P<T> = Promise<INTERNAL_RES_ARR<T>>;
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,1 +1,49 @@
+import { OBJECT_TYPE } from "../types/common";
+
+// REF: https://developers.cloudflare.com/workers/examples/read-post/
+export const read_req_body = async (request: Request): Promise<{ success: true, type: "json", body: OBJECT_TYPE<any> } | { success: true, type: "text", body: string } | { success: false, errors: string[] }> => {
+    const contentType = request.headers.get("content-type");
+    if (contentType?.includes("application/json")) {
+        return {
+            success: true,
+            type: "json",
+            body: await request.json(),
+        };
+    }
+    else if (contentType?.includes("application/text")) {
+        return {
+            success: true,
+            type: "text",
+            body: await request.text(),
+        };
+    }
+    else if (contentType?.includes("text/html")) {
+        return {
+            success: true,
+            type: "text",
+            body: await request.text(),
+        };
+    }
+    else if (contentType?.includes("form")) {
+        const formData = await request.formData();
+        const body: OBJECT_TYPE<any> = {};
+        for (const entry of formData.entries()) {
+            body[entry[0]] = entry[1];
+        }
+        return {
+            success: true,
+            type: "json",
+            body,
+        };
+    }
+    else {
+        // Perhaps some other type of data was submitted in the form
+        // like an image, or some other binary data.
+        return {
+            success: false,
+            errors: ["no content type specified"]
+        };
+    }
+}
+
 export default {};
